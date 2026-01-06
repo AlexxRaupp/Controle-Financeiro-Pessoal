8bdfe1489ffe1a261f47667a88831d2849948b4c
Alex Raupp <142125391+AlexxRaupp@users.noreply.github.com>
2026-01-06 15:26:15 -0300

    erro em cadastrar

--- js/firebase.js
+++ js/firebase.js
@@ -3,15 +3,17 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGIQvXEf8UwCH86zMU2mjxBIvyQ9xSXKM",
+  apiKey: "AIzaSyAGIQVXF8UwCH86zMU2mjxBIvyQ9xSXKM",
   authDomain: "controle-financeiro-e7c6e.firebaseapp.com",
   projectId: "controle-financeiro-e7c6e",
   storageBucket: "controle-financeiro-e7c6e.appspot.com",
   messagingSenderId: "734829903918",
-  appId: "1:734829903918:web:891d9fdf3d42974c38dfbf"
+  appId: "1:734829903918:web:801d9fdf3d42074c38dfbf"
 };
 
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exporta serviços
 export const auth = getAuth(app);
 export const db = getFirestore(app);
