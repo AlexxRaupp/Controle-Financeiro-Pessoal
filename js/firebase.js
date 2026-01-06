c4fdd2739b50d91044be04625d6e4701617b7050
Alex Raupp <142125391+AlexxRaupp@users.noreply.github.com>
2026-01-06 15:30:26 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAGIQVXF8UwCH86zMU2mjxBIvyQ9xSXKM",
+  apiKey: "AIzaSyAGIQVxEf8UwcH86zMU2mjxBIbyQ9xSXKM",
   authDomain: "controle-financeiro-e7c6e.firebaseapp.com",
   projectId: "controle-financeiro-e7c6e",
-  storageBucket: "controle-financeiro-e7c6e.appspot.com",
+  storageBucket: "controle-financeiro-e7c6e.firebasestorage.app",
   messagingSenderId: "734829903918",
-  appId: "1:734829903918:web:801d9fdf3d42074c38dfbf"
+  appId: "1:734829903918:web:801d9fdf3d42074c38dfbf",
+  measurementId: "G-BG3JTSZSJG"
 };
-
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
-
-// Exporta serviços
-export const auth = getAuth(app);
-export const db = getFirestore(app);
